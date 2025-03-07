60e626bbbb5de1c29eb5cb91e698a03e67e8765d
Artur Zelenco <my1lettersbox@yahoo.com>
2025-03-07 16:16:58 +0200

    feat: function construct

--- Module-8/Telegraph.php
+++ Module-8/Telegraph.php
@@ -7,4 +7,12 @@ class TelegraphText {
     public string $published;
     public string $slug ='test_text_file';
     const FILE_EXTENSION = '.txt';
-}
+    
+    public function __construct(string $title, string $author, string $text) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->text = $text;
+        $this->published = date('Y-m-d H:i:s');
+        $this->slug = str_replace(' ', '-', $title);
+            }
+        }
