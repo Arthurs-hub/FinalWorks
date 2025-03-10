e3971a4f8c8406eba4f6895e7574513fc80d06d9
Artur Zelenco <my1lettersbox@yahoo.com>
2025-03-10 13:21:35 +0200

    feat: function store text

--- Module-8/Telegraf.php
+++ Module-8/Telegraf.php
@@ -8,4 +8,27 @@ class TelegraphText
     public string $published;
     public string $slug = 'test_text_file';
     const FILE_EXTENSION = '.txt';
+
+    public function __construct(string $title, string $author, string $text)
+    {
+        $this->title = $title;
+        $this->author = $author;
+        $this->text = $text;
+        $this->published = date('Y-m-d H:i:s');
+        $this->slug = str_replace(' ', '-', $title);
+    }
+
+    public function storeText(): string
+    {
+        $data = [
+            'title' => $this->title,
+            'text' => $this->text,
+            'author' => $this->author,
+            'published' => $this->published
+        ];
+        $serializedData = serialize($data);
+        file_put_contents('test_text_file' . self::FILE_EXTENSION, $serializedData);
+        return $this->slug;
+    }
 }    
+    
