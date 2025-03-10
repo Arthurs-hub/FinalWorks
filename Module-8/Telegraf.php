dc526d77b6fb7aae83eacbb9c94306d00f7ddaa4
Artur Zelenco <my1lettersbox@yahoo.com>
2025-03-10 13:14:30 +0200

    feat: add class TelegraphText

---
+++ Module-8/Telegraf.php
@@ -0,0 +1,11 @@
+<?php
+
+class TelegraphText
+{
+    public string $title;
+    public string $text;
+    public string $author;
+    public string $published;
+    public string $slug = 'test_text_file';
+    const FILE_EXTENSION = '.txt';
+}    
