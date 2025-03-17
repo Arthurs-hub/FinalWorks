cdb0270d30eae31719fdb1cbe20f6b54f305e7a3
Artur Zelenco <my1lettersbox@yahoo.com>
2025-03-17 10:32:39 +0200

    feat: new TelegraphText

--- Module-10/TelegraphTemplates.php
+++ Module-10/TelegraphTemplates.php
@@ -70,3 +70,21 @@ class Spl extends View
         return $templateContent;
     }
 }
+
+$telegraphText = new TelegraphText('Vasya', 'Some-slug');
+$telegraphText->editText('Some title', 'Some text');
+
+$swig = new Swig('telegraph_text');
+$swig->addVariablesToTemplate(['slug', 'text']);
+
+$spl = new Spl('telegraph_text');
+$spl->addVariablesToTemplate(['slug', 'title', 'text']);
+
+$templateEngines = [$swig, $spl];
+foreach ($templateEngines as $engine) {
+    if ($engine instanceof IRender) {
+        echo $engine->render($telegraphText) . PHP_EOL;
+    } else {
+        echo 'Template engine does not support render interface' . PHP_EOL;
+    }
+}
