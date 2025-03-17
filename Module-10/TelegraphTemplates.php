0c9495f29ffb952ac1bb56119f6ea4931010bb0b
Artur Zelenco <my1lettersbox@yahoo.com>
2025-03-17 10:24:27 +0200

    feat: abstract class View

--- Module-10/TelegraphTemplates.php
+++ Module-10/TelegraphTemplates.php
@@ -1,22 +1,42 @@
 <?php
 
-class TelegraphText {
+class TelegraphText
+{
     public string $title;
     public string $text;
     public string $slug;
 
-    public function __construct(string $title, string $text) {
+    public function __construct(string $title, string $text)
+    {
         $this->title = $title;
         $this->text = $text;
         $this->slug = 'Some slug';
     }
 
-    public function editText(string $title, string $text): void {
+    public function editText(string $title, string $text): void
+    {
         $this->title = $title;
         $this->text = $text;
     }
 }
 
-interface IRender {
+interface IRender
+{
     public function render(TelegraphText $telegraphText): string;
+}
+
+abstract class View implements IRender
+{
+    protected string $templateName;
+    protected array $variables = [];
+
+    public function __construct(string $templateName)
+    {
+        $this->templateName = $templateName;
+    }
+
+    public function addVariablesToTemplate(array $variables): void
+    {
+        $this->variables = $variables;
+    }
 }
