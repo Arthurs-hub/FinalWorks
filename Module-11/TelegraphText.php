7992d33db918a5891c629593c751d3e2649d0f09
Artur Zelenco <my1lettersbox@yahoo.com>
2025-03-19 10:40:59 +0200

    feat: function __set

--- Module-11/TelegraphText.php
+++ Module-11/TelegraphText.php
@@ -8,4 +8,32 @@ class TelegraphText
     private $published = '';
     private $slug = '';
 
-}    
+    public function __set($name, $value)
+    {
+        switch ($name) {
+            case 'author':
+                if (strlen($value) > 120) {
+                    throw new Exception("Author name cannot be longer than 120 characters.");
+                }
+                $this->author = $value;
+                break;
+            case 'slug':
+                if (!preg_match('/^[a-zA-Z0-9_-]+$/', $value)) {
+                    throw new Exception("Slug can only contain letters, numbers, hyphens, and underscores.");
+                }
+                $this->slug = $value;
+                break;
+            case 'published':
+                if (strtotime($value) < time()) {
+                    throw new Exception("Published date must be greater than or equal to the current date.");
+                }
+                $this->published = $value;
+                break;
+            case 'text':
+                $this->storeText($value);
+                break;
+            default:
+                throw new Exception("Invalid property: " . $name);
+        }
+    }
+}
