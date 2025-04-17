894056c91f6eea0bfbb0a103812f18d8cb44e192
Artur Zelenco <my1lettersbox@yahoo.com>
2025-04-17 12:39:10 +0300

    feat: folder Interfaces with Irender file and namespace

---
+++ Module-13/Telegraph/Interfaces/IRender.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Telegraph\Interfaces;
+use Telegraph\Entities\TelegraphText;
+interface IRender
+    {
+        public function render(TelegraphText $telegraphText): string;
+    }
+
+    
+
