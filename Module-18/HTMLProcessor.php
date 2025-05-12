3fec7ba5cfd4b37dca1e76534ff118c5a842519a
Artur Zelenco <my1lettersbox@yahoo.com>
2025-05-12 12:32:12 +0300

    feat:updated HTMLProcessor

--- Module-18/HTMLProcessor.php
+++ Module-18/HTMLProcessor.php
@@ -20,25 +20,58 @@ function getHTMLFromURL($url)
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    $html = curl_exec($ch);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    $response = curl_exec($ch);
+
+    $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $headers = substr($response, 0, $headerSize);
+    $body = substr($response, $headerSize);
+
+
+    if (preg_match('/charset=([\w-]+)/i', $headers, $matches)) {
+        $charset = $matches[1];
+        if (strtolower($charset) !== 'utf-8') {
+            $body = iconv($charset, 'UTF-8//IGNORE', $body);
+        }
+    }
+
     curl_close($ch);
-    return $html;
+    return $body;
 }
 
-function parseImagesFromHTML($html)
+function parseImagesFromHTML($html, $baseUrl)
 {
     $doc = new DOMDocument();
-    @$doc->loadHTML($html); 
+
+    $html = trim($html);
+
+    libxml_use_internal_errors(true);
+    @$doc->loadHTML($html);
+    libxml_clear_errors();
+
     $tags = $doc->getElementsByTagName('img');
     $images = [];
     foreach ($tags as $tag) {
-        $images[] = $tag->getAttribute('src');
+        $src = $tag->getAttribute('src');
+        if (filter_var($src, FILTER_VALIDATE_URL)) {
+            $images[] = $src;
+        } else {
+            $images[] = rtrim($baseUrl, '/') . '/' . ltrim($src, '/');
+        }
     }
     return $images;
 }
 
 $html = getHTMLFromURL($url);
-$images = parseImagesFromHTML($html);
+$html = trim($html);
+
+if (empty($html)) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Failed to fetch HTML from the URL']);
+    exit;
+}
+
+$images = parseImagesFromHTML($html, $url);
 
 if (!empty($images)) {
     http_response_code(200);
